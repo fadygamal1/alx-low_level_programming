6d4d1424376cfbfd87e03babc32e4a3e484e6e2d
fadygamal1 <fadygamal007@gmail.com>
2023-09-12 03:16:06 -0700

    commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,13 +5,22 @@
  * @n: the int to check
  * Return: 1 and prints + if n is greater than zero
  * 0 and prints 0 if n is zero
- * -1 and prints - if n is less than zero
- */
-int print_sign(int n)
+ * -1 and prints - if n is less 
+ **/
+t_sign(int n)
+int prinif (n >0)
 {
-if (n > 0)
+-putchar(13);
+return (1);
+}
+else if (n == 0)
 {
-_putchar('+');
-return(1);
-} else if (n == 0)
+-putchar(48);
+return (0);
+}
+else
+{
+_putchar(45);
+return (-1);
+}
 }
