88a44050f0fc620b19da9b38b5513db83a49a6ea
fadygamal11 <fadygamal007@gmail.com>
2023-10-19 11:10:04 -0700

    commit

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,12 +1,13 @@
 #ifndef LISTS_H
 #define LISTS_H
+
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
+ * struct list_s - Singly linked list
+ * @str: String (malloc'ed string)
+ * @len: Length of the string
+ * @next: Points to the next node
  *
- * Description: singly linked list node structure
+ * Description: Singly linked list node structure
  */
 typedef struct list_s
 {
